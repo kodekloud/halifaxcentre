d06569d2e9352dcc0779289968d578a7c81ff2bf
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-13 00:07:37 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -1,10 +1,31 @@
-    function sortByDate(a, b){
+ function renderBanner(banner_template, home_banner, repo){
+    var item_list = [];
+    var banner_template_html = $(banner_template).html();
+    Mustache.parse(banner_template_html);   // optional, speeds up future uses
+    var count = 0;
+    $.each( repo , function( key, val ) {
+        if( val.name == "banner"){
+            $.each( val.images , function( key, val ) {
+                var repo_rendered = Mustache.render(banner_template_html,val);
+                item_list.push(repo_rendered);
+                count += 1;
+            });
+         
+        }
+    });
+    
+    $(home_banner).show();
+    $(home_banner).html(item_list.join(''));
+    $('.item').first().addClass('active');
+}
+
+function sortByDate(a, b){
        
-        var aDate = a.publish_date;
-        var bDate = b.publish_date;
- 
-        return ((aDate > bDate) ? -1 : ((aDate < bDate) ? 1 : 0));
-    }
+    var aDate = a.publish_date;
+    var bDate = b.publish_date;
+
+    return ((aDate > bDate) ? -1 : ((aDate < bDate) ? 1 : 0));
+}
     
 function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,empty_section_id,events){
     var item_list = [];
