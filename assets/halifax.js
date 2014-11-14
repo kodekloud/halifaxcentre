713f0b7c0ec53a2c926131d2c34a501a7368b6be
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-14 10:58:26 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -35,16 +35,15 @@
 
 
 
-function renderEventWithImgTemplate(template_id,html_id,not_empty_section_id,empty_section_id,events, type){
+function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,empty_section_id,events, type){
     var item_list = [];
     var template_html = $(template_id).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
 
-    console.log('hi');
+
     $.each( events , function( key, val ) {
         localizeObject(val);
-        alert(val.tags.length);
-        if(val.tags.length === 0){
+        if(val.tags == type){
             if(hasImage(val.event_image_url)){
                 val.event_image_url = getImageURL(val.event_image_url);
                 val.event_image_url_abs = getAbsoluteImageURL(val.event_image_url_abs);
@@ -228,34 +227,7 @@ function renderStoreWithImgTemplate(template_id,html_id,not_empty_section_id,emp
     }     
      
         
-        
-function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,empty_section_id,events){
-    var item_list = [];
-    var template_html = $(template_id).html();
-    Mustache.parse(template_html);   // optional, speeds up future uses
 
-    
-    $.each( events , function( key, val ) {
-        localizeObject(val);
-        if(hasImage(val.event_image_url)){
-            val.event_image_url = getImageURL(val.event_image_url);
-            val.event_image_url_abs = getAbsoluteImageURL(val.event_image_url_abs);
-    
-        }else{
-            val.event_image_url =  "http://kodekloud.s3.amazonaws.com/sites/5438407c6e6f64462d020000/bc66d880720f58f49b267ae6fb920f74/default.jpg";
-        }
-        var rendered = Mustache.render(template_html,val);
-        item_list.push(rendered);
-    });
-    if(events.length > 0){
-        $(not_empty_section_id).show();
-        $(empty_section_id).hide();
-        $(html_id).html(item_list.join(''));
-    }else{
-        $(not_empty_section_id).hide();
-        $(empty_section_id).show();
-    }
-}
 function sortByDate(a, b){
        
     var aDate = a.publish_date;
