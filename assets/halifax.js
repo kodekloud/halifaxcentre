0d733f2edbcfa0b2f2a23c2c62edd120d52494f5
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-21 16:09:51 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -111,7 +111,7 @@ function renderStoreWithImgTemplate(template_id,html_id,not_empty_section_id,emp
         localizeObject(val);
         var promotionable_name = "";
         var promotionable_url = "";
-      if(val['promotionable_type'] == 'Store' && showOnWeb(val)){
+      if(val['promotionable_type'] == 'Store' ){
             var store_details = getStoreDetailsByID(val['promotionable_id']);
             if (store_details){
                 localizeObject(store_details);
