87dbb3a9b9c5609aca9e5027ab27c0fb60e63942
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-24 10:38:53 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -32,30 +32,6 @@
 }
 
 
-function showOnWeb(eventObj){
-    var showDate = eventObj.show_on_web_date;
-    var dateParts = showDate.split("-");
-    
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-    
-    if(dd<10) {
-        dd='0'+dd
-    } 
-    
-    if(mm<10) {
-        mm='0'+mm
-    } 
-    
-    today = yyyy+'-'+ mm +'-'+ dd;
-    if (today >= showDate){
-       return true;
-    } else {
-        return false;
-    }
-}
 
 
 function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,empty_section_id,events, type){
