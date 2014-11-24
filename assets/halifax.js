293ecbeb950dd2a8931b69a7b0a16962717dfe5f
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-24 11:17:34 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -98,10 +98,9 @@ function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,em
 
     $.each( events , function( key, val ) {
         localizeObject(val);
-         console.log(val.tags);
-        console.log($.inArray(type, val.tags));
+
         
-        if(val.tags == type && showOnWeb(val)){
+        if($.inArray(type, val.tags) != -1 && showOnWeb(val)){
             if(hasImage(val.event_image_url)){
                 val.event_image_url = getImageURL(val.event_image_url);
                 val.event_image_url_abs = getAbsoluteImageURL(val.event_image_url_abs);
