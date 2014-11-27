e708ab9be3c1dc97c7095861dd77f1e60f4347c2
mark@mobilefringe.com <mark@mobilefringe.com>
2014-11-27 11:44:22 -0500

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -100,7 +100,7 @@ function renderPropertyStorePromotionsListTemplate(template_id,template_id_no_im
         item_list.sort(sortByDate);
        sorted_list = renderingObj(item_list, toHtml);
     
-    console.log(sorted_list);
+    console.log(toHtml);
     if(promotions.length > 0){
         $(not_empty_section_id).show();
         $(empty_section_id).hide();
