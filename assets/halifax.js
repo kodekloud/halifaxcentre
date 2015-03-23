ed369a9562fc6ff75c7dd3b0c9dfac8e45b0eb47
mark@mobilefringe.com <mark@mobilefringe.com>
2015-03-22 21:04:18 -0400

    via kodekloud.io (54643b4d6e6f643214000000)

--- assets/halifax.js
+++ assets/halifax.js
@@ -260,7 +260,7 @@ function renderEventsWithImgTemplate(template_id,html_id,not_empty_section_id,em
                 }else{
                     val.show = '';
                 }
-                val.event_image_url =  "http://kodekloud.s3.amazonaws.com/sites/5438407c6e6f64462d020000/bc66d880720f58f49b267ae6fb920f74/default.jpg";
+                val.event_image_url =  "http://kodekloud.s3.amazonaws.com/sites/5438407c6e6f64462d020000/d17d8c90fe9a800df686233fab0e6569/default.jpg";
                  
             }
             item_list.push(val);        
