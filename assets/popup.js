ec7edc133da1f04cf96e1c15240aff7c18e613cd
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 14:20:09 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -49,6 +49,7 @@ function popup(page){
 function popupSubmit(){
     	var ajaxUrl = "http://halifaxcentre.mallmaverickstaging.com/create_popup_contest_entry/";
     	var contest = {};
+    	contest['id'] = 5;
         	$.ajax({
                 url: ajaxUrl,
                 type: "POST",
