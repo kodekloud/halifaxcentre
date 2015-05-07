008b1f6b3a3e297480338a6144d0dcb90d975130
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 17:22:36 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -51,24 +51,3 @@ function popup(page){
         });     
      }
 }
-function popupSubmit(){
-    var ajaxUrl = "http://halifaxcentre.mallmaverickstaging.com/create_popup_contest_entry/";
-	var contest = {};
-	contest['contest_id'] = 5;
-	contest['first_name'] = 'Dragon';
-	var raj ={};
-	raj['contest'] = contest; 
-	$.ajax({
-        url: ajaxUrl,
-        type: "POST",
-        
-        data:raj,
-    	success: function(response){                        
-		    alert('success');
-		},
-        error: function(xhr, ajaxOptions, thrownError){
-            
-            alert("Please try again later.");
-		}
-    });
-}
