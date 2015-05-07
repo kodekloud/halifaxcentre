c4b4398b154453100f9c25741b30ad8fef1f970c
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 11:28:55 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -1,3 +1,6 @@
+
+//takes sting parameter that wish to show the popup
 function popup(page){
-    
+    var pathArray = window.location.pathname.split( '/' );
+    var slug = pathArray[pathArray.length-1];
 }
