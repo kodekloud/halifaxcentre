9a4886883f32e9e6eec3f0858fe09fe8b5fb2ffd
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 17:23:09 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -1,6 +1,6 @@
 
 //takes sting parameter that wish to show the popup
-function popup(page){
+function popupCheck(page){
     
     var pathArray = window.location.pathname.split( '/' );
     var slug = pathArray[pathArray.length-1];
@@ -50,4 +50,25 @@ function popup(page){
             event.stopPropagation();
         });     
      }
+}
+function popupSubmit(){
+    var ajaxUrl = "http://halifaxcentre.mallmaverickstaging.com/create_popup_contest_entry/";
+	var contest = {};
+	contest['contest_id'] = 5;
+	contest['first_name'] = 'Dragon';
+	var raj ={};
+	raj['contest'] = contest; 
+	$.ajax({
+        url: ajaxUrl,
+        type: "POST",
+        
+        data:raj,
+    	success: function(response){                        
+		    alert('success');
+		},
+        error: function(xhr, ajaxOptions, thrownError){
+            
+            alert("Please try again later.");
+		}
+    });
 }
