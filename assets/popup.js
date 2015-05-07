6d46c450e9c3cb67ea30043a666d41439c6fb40f
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 13:11:00 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -47,7 +47,7 @@ function popup(page){
      }
 }
 function popupSubmit(){
-    
+        
         $.getJSON(
             "http://mallmaverickstaging.com/create_popup_contest_entry?",
            $("#popup_signup").serialize(),
