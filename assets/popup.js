d4da342d9ce4a8ddce39da6bf9b9ea2af424f792
mark@mobilefringe.com <mark@mobilefringe.com>
2015-05-07 11:40:17 -0400

    via kodekloud.io (554b843d6e6f64338b000000)

--- assets/popup.js
+++ assets/popup.js
@@ -8,7 +8,7 @@ function popup(page){
         var visited = $.cookie("popup");
         var winwidth = $(window).width();
         if(visited === null){ visited = 1; $.cookie('popup', visited, { expires: 7 }); }
-        if (visited <= 3) {
+        if (visited <= 100) {
             if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || winwidth <= 600) {
                 $(".hidden-popup-bg").show();
             }else{
